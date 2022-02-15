ebfa64ddb2d640ce717a30c9240a5545344a2da5
RomanSteele <romanzelenenko@yandex.ru>
2022-02-15 15:57:38 +0300

    module2-task1 wip

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Settings = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  title = {Settings.title} genre = {Settings.genre} year = {Settings.year} />
   </React.StrictMode>,
   document.getElementById('root'));
